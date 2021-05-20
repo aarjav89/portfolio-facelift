c1cd3111febd3b59af651c711aff2851886824fa
aarjav89 <aarjav.chandarana@gmail.com>
2021-05-20 15:05:17 -0400

    Added Particle JS effect to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import Home2 from "./Home2";
+import Particle from "../Particle";
+
 
 const Home = (props) => {
     return (
-        <div>This is Home Component - Sec 1</div>
+        <section>
+            <Container fluid className="home-section" id="home">
+                <Particle/>
+            </Container>
+        </section>
+
     )
 }
 
