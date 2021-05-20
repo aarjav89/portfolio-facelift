c1e4cc56785b8139415a838721228f7ddb0e21d4
aarjav89 <aarjav.chandarana@gmail.com>
2021-05-20 17:06:08 -0400

    Completed Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = (props) => {
+    let date = new Date();
+    let year = date.getFullYear();
     return (
-        <div>
-            <h1> This is the Footer </h1>
-        </div>
+        <Container fluid className="footer">
+            <Row>
+                <Col md="4" className="footer-copyright">
+                    <h3>Designed and Developed by : Aarjav Chandarana</h3>
+                </Col>
+                <Col md="4" className="footer-copyright">
+                    <h3>Copyright © {year} AC</h3>
+                </Col>
+                <Col md="4" className="footer-body">
+                    <ul className="footer-icons">
+                        <li className="social-icons">
+                            <a
+                                href="https://github.com/aarjav89"
+                                style={{ color: "white" }}
+                            >
+                                <AiFillGithub />
+                            </a>
+                        </li>
+                        <li className="social-icons">
+                            <a
+                                href="https://twitter.com/aarjav89"
+                                style={{ color: "white" }}
+                            >
+                                <AiOutlineTwitter />
+                            </a>
+                        </li>
+                        <li className="social-icons">
+                            <a
+                                href="https://www.linkedin.com/in/aarjav-c/"
+                                style={{ color: "white" }}
+                            >
+                                <FaLinkedinIn />
+                            </a>
+                        </li>
+
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
---
+++ src/components/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = (props) => {
+    return (
+        <div id={props.load ? "preloader" : "preloader-none"}>
+
+        </div>
+    );
+}
+
+export default Preloader;
