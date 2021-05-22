3b7db67776a9dfd4d610900031670b2ddda7cf4d
aarjav89 <aarjav.chandarana@gmail.com>
2021-05-22 10:07:46 -0400

    Completed Techstack component

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
 
 const AboutCard = (props) => {
     return (
-        <div></div>
+        <Card className="quote-card-view">
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <p style={{ textAlign: "justify" }}>
+                        Hello, I am <span className="purple">Aarjav Chandarana </span>
+                        from <span className="purple"> Toronto, ON</span>
+                        <br />I am a professional Full Stack Developer.
+                        <br />
+                        <br />
+                        Apart from coding, some other activities that I love to do!
+                    </p>
+                    <ul>
+                        <li className="about-activity">
+                            <ImPointRight /> Solving Puzzles on Unblock
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight /> Writting Tech Blogs
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight /> Photography
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight /> Hiking
+                        </li>
+                    </ul>
+
+                    <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
+                        "Life is like an ice-cream! Enjoy before it melts"{" "}
+                    </p>
+                    <footer className="blockquote-footer">Aarjav</footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,8 +1,57 @@
 import React from 'react'
+import { Col, Row } from "react-bootstrap";
+import { FaVuejs, FaLaravel } from "react-icons/fa";
+
+import {
+
+    DiJavascript1,
+    DiReact,
+    DiNodejs,
+    DiMongodb,
+    DiPython,
+    DiGit,
+} from "react-icons/di";
+import { SiMysql, SiFirebase, SiTailwindcss } from "react-icons/si";
 
 const Techstack = (props) => {
     return (
-        <div></div>
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            <Col xs={4} md={2} className="tech-icons">
+                <FaVuejs/><p> Vue.js </p>
+            </Col>
+
+            <Col xs={4} md={2} className="tech-icons">
+                <DiReact /><p> React </p>
+            </Col>
+
+            <Col xs={4} md={2} className="tech-icons">
+                <DiNodejs /><p> Node.js </p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiJavascript1 /><p> Javascript</p>
+            </Col>
+
+            <Col xs={4} md={2} className="tech-icons">
+                <FaLaravel/><p> Laravel </p>
+            </Col>
+
+
+            <Col xs={4} md={2} className="tech-icons">
+                <DiMongodb /><p>MongoDB</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiMysql /><p>MySql</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiTailwindcss /><p> Tailwind CSS </p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiGit /><p> Git </p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiFirebase /><p> FireBase</p>
+            </Col>
+        </Row>
     )
 }
 
