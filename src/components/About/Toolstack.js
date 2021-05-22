ef2a05447e0aee568f3773ae94b67304a0b6e89c
aarjav89 <aarjav.chandarana@gmail.com>
2021-05-22 10:13:28 -0400

    Completed Toolstack component

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Col, Row } from "react-bootstrap";
+import {
+    SiVisualstudiocode,
+    SiPostman,
+    SiWebstorm
+} from "react-icons/si";
+import { FaAws, FaDigitalOcean } from "react-icons/fa";
 
 const Toolstack = (props) => {
     return (
-        <div></div>
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiWebstorm/><p>WebStorm</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiVisualstudiocode /><p> VS Code </p>
+            </Col>
+
+            <Col xs={4} md={2} className="tech-icons">
+                <SiPostman /><p>Postman</p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <FaAws /><p> AWS </p>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <FaDigitalOcean /><p> Digital Ocean </p>
+            </Col>
+        </Row>
     )
 }
 
